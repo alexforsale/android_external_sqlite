2680494efdc919149869b9f691b176e80249647c
David Ng <dave@codeaurora.org>
2011-07-05 10:53:34 -0700

    Add hooks for database open and pragma handling

--- dist/Android.mk
+++ dist/Android.mk
@@ -37,6 +37,11 @@ LOCAL_SHARED_LIBRARIES += liblog \
 # include android specific methods
 LOCAL_WHOLE_STATIC_LIBRARIES := libsqlite3_android
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+LOCAL_WHOLE_STATIC_LIBRARIES += libqc-sqlite
+LOCAL_SHARED_LIBRARIES += libcutils
+endif
+
 ## Choose only one of the allocator systems below
 # new sqlite 3.5.6 no longer support external allocator 
 #LOCAL_SRC_FILES += mem_malloc.c
